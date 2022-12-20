964f2b9a382ac2a39b43b5c9fca703605ee48ac7
1-Hee <onehee9710@gmail.com>
2022-12-20 18:14:59 +0900

    12/20 18:13 | 결제 페이지 추가, 결제 페이지 모달창 추가, 카테고리 ~ 결제 까지 Vanilla JavaScritp로 화면 이동 임시 구현

---
+++ kiosk_web_publishing_src/assets/js/pay-result.js
@@ -0,0 +1,11 @@
+document.querySelector(".back-menu-btn").addEventListener("click", () => {
+  location.href = "/menu-view.html";
+});
+
+document.querySelector(".pay-cancel-btn").addEventListener("click", () => {
+  location.href = "/category.html";
+});
+
+document.querySelector(".do-pay-btn").addEventListener("click", () => {
+  alert("서비스 준비중입니다!");
+});
